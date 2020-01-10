79815b72470ac2e7d0746c3d51452648912429d5
Brandon Lax <brlax@optonline.net>
2020-01-09 22:50:04 -0500

    Added parameters for most methods

--- Jsonify.java
+++ Jsonify.java
@@ -56,6 +56,7 @@ public class Jsonify {
       } else {
          throw new Exception("Missing base tests tag.");
       }
+      pw.println("gr.testRunFinished();");
       pw.println("\t}\n}");
       pw.flush();
       pw.close();
@@ -76,7 +77,9 @@ public class Jsonify {
             throw new Exception("There does not exist a test for the type: " + type);
          }
          String method = testMap.get(type);
-         String test = "gr." + method + "()";
+         String parameters = getParams(type, params);
+         
+         String test = "gr." + method + "(" + parameters +")";
          pw.printf("gr.setScore(%f);\n", score.doubleValue());
          pw.printf("gr.setVisibility(%d);\n", visibility);
          if (condition.contains(type) && obj.containsKey("conditiontests") && obj.get("conditiontests") != null) {
@@ -88,5 +91,159 @@ public class Jsonify {
             pw.printf("%s;\n", test);
          }
       }
+   }
+
+   public static String getParams(String type, JSONObject params) throws Exception {
+      String file, name, log, sample, input, typeName;
+      Boolean field;
+      Number count, points, visCount;
+      switch(type) {
+        case "noarraylist":
+        case "nopackage":
+        case "fileexists":
+        case "junittests":
+        case "compiles":
+        case "nopiv":
+        case "checkstylepf":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           file = (String) params.get("classname");
+           return "\"" + file + "\"";
+        case "constructcount":
+        case "methodcount":
+        case "comptests":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("count")) {
+              throw new Exception("Test of type " + type + " does not have parameter count");
+           }
+           file = (String) params.get("classname");
+           count = (Number) params.get("count");
+           return "\"" + file + "\", " + count;
+        case "comptest":
+           return "";
+        case "compfiles":
+           if (!params.containsKey("first")) {
+              throw new Exception("Test of type " + type + " does not have parameter first");
+           }
+           if (!params.containsKey("second")) {
+              throw new Exception("Test of type " + type + " does not have parameter second");
+           }
+           String first = (String) params.get("first");
+           String second = (String) params.get("second");
+           return "\"" + first + "\", \"" + second + "\"";
+        case "hasfield":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("classtype")) {
+              throw new Exception("Test of type " + type + " does not have parameter classtype");
+           }
+           if (!params.containsKey("field")) {
+              throw new Exception("Test of type " + type + " does not have parameter field");
+           }
+           name = (String) params.get("classname");
+           typeName = (String) params.get("classtype");
+           String fieldName = (String) params.get("field");
+           return "\"" + name + "\", \"" + fieldName + "\", \"" + typeName +"\"";
+        case "hasmethod":
+           return "";
+        case "logdifftest":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("logname")) {
+              throw new Exception("Test of type " + type + " does not have parameter logname");
+           }
+           if (!params.containsKey("samplename")) {
+              throw new Exception("Test of type " + type + " does not have parameter samplename");
+           }
+           if (!params.containsKey("inputname")) {
+              throw new Exception("Test of type " + type + " does not have parameter inputname");
+           }
+           name = (String) params.get("classname");
+           log = (String) params.get("logname");
+           sample = (String) params.get("samplename");
+           input = (String) params.get("inputname");
+           return "\"" + name + "\", \"" + log + "\", \"" + sample + "\", \"" + input +"\"";
+        case "logdifftests":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("logname")) {
+              throw new Exception("Test of type " + type + " does not have parameter logname");
+           }
+           if (!params.containsKey("samplename")) {
+              throw new Exception("Test of type " + type + " does not have parameter samplename");
+           }
+           if (!params.containsKey("count")) {
+              throw new Exception("Test of type " + type + " does not have parameter count");
+           }
+           name = (String) params.get("classname");
+           log = (String) params.get("logname");
+           sample = (String) params.get("samplename");
+           count = (Number) params.get("count");
+           visCount = 0;
+           if (params.containsKey("viscount")) {
+              visCount = (Number) params.get("viscount");
+           }
+           return "\"" + name + "\", " + count.intValue() + ", " + visCount.intValue() +", \"" + log + "\", \"" + sample + "\"";
+        case "stddifftest":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("count")) {
+              throw new Exception("Test of type " + type + " does not have parameter count");
+           }
+           if (!params.containsKey("sample")) {
+              throw new Exception("Test of type " + type + " does not have parameter sample");
+           }
+           name = (String) params.get("classname");
+           count = (Number) params.get("count");
+           field = (Boolean) params.get("sample");
+           return  "\"" + name + "\", " + count.intValue() + ", " + field;
+        case "stddifftests":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("count")) {
+              throw new Exception("Test of type " + type + " does not have parameter count");
+           }
+           if (!params.containsKey("sample")) {
+              throw new Exception("Test of type " + type + " does not have parameter sample");
+           }
+           name = (String) params.get("classname");
+           count = (Number) params.get("count");
+           field = (Boolean) params.get("sample");
+           String line =  "\"" + name + "\", " + count.intValue() + ", " + field;
+           if (params.containsKey("viscount")) {
+              visCount = (Number) params.get("viscount");
+              line +=  ", " + visCount.intValue();
+           }
+           return line;
+        case "checkstylepererr":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("pointper")) {
+              throw new Exception("Test of type " + type + " does not have parameter pointper");
+           }
+           file = (String) params.get("classname");
+           points = (Number) params.get("pointper");
+           return "\"" + file + "\", " + points + ", false";
+        case "checkstylepertype":
+           if (!params.containsKey("classname")) {
+              throw new Exception("Test of type " + type + " does not have parameter classname");
+           }
+           if (!params.containsKey("pointper")) {
+              throw new Exception("Test of type " + type + " does not have parameter pointper");
+           }
+           file = (String) params.get("classname");
+           points = (Number) params.get("pointper");
+           return "\"" + file + "\", " + points + ", false";
+      }
+      return "";
    }
 }
